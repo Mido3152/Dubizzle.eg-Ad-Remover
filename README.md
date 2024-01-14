a3ae29a7ce17708b97270bd27e20cbcfafcaca71
Ahmed Elsaeed <Midmido3152003@gmail.com>
2024-01-14 19:44:40 +0200

    main branch

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dubizzle.eg Ad Remover
+
+This is my attempt at a browser extension that completely removes paid-for ads from dubizzle.eg, so that all ads are by real, genuine people looking to sell their property.
+
+## Installation
+
+- Download the [latest release]().
+
+## Building From Source
+
+- Clone the GitHub repo.
+  bash`git clone `
+  -Go into your browser's extension menu and enable developer mode.
+
+- Either `load unpacked`, or `pack extension` and refer to the Installation section.
---
+++ content.js
@@ -0,0 +1,65 @@
+removeads();
+
+function removeads() {
+  var listings = document.querySelectorAll('li[aria-label^="Listing"]');
+  var footerAds = document.querySelectorAll('div[class^="f2b12cf8"]');
+  var propertyBannerAds = document.querySelectorAll(
+    'div[aria-label^="Listing"]'
+  );
+
+  for (let i = 0; i < listings.length; i++) {
+    if (
+      listings[i]
+        .getElementsByTagName("article")[0]
+        .getElementsByTagName("div")[0]
+        .querySelector('div[aria-label^="Featured"') ||
+      listings[i]
+        .getElementsByTagName("article")[0]
+        .getElementsByTagName("div")[0]
+        .querySelector('div[aria-label^="Elite"')
+    ) {
+      listings[i].setAttribute("hidden", "");
+    }
+  }
+  for (let i = 0; i < footerAds.length; i++) {
+    if (
+      footerAds[i]
+        .getElementsByTagName("article")[0]
+        .getElementsByTagName("div")[0]
+        .querySelector('div[aria-label^="Featured"')
+    ) {
+      footerAds[i].setAttribute("hidden", "");
+    }
+  }
+  for (let i = 0; i < propertyBannerAds.length; i++) {
+    if (
+      propertyBannerAds[i]
+        .getElementsByTagName("article")[0]
+        .getElementsByTagName("div")[0]
+        .querySelector('div[aria-label^="Featured"')
+    ) {
+      propertyBannerAds[i].setAttribute("hidden", "");
+    }
+  }
+}
+removeads();
+async function run() {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      removeads();
+    });
+  });
+
+  const targetElement = document.getElementsByTagName("head")[0];
+
+  observer.observe(targetElement, {
+    attributes: true,
+    characterData: true,
+    childList: true,
+    subtree: true,
+    attributeOldValue: true,
+    characterDataOldValue: true,
+  });
+}
+
+run();
