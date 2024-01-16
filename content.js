f48475726a2c85fdbdcb286cbb09083675cc5aba
Ahmed Elsaeed <Midmido3152003@gmail.com>
2024-01-16 04:27:03 +0200

    Added comments.

--- content.js
+++ content.js
@@ -1,13 +1,14 @@
-console.log("Added");
+// Runs the removeads() function once during initial loading of the page.
 removeads();
 
 function removeads() {
+  // Load all listings and banners that might contain ads.
   var listings = document.querySelectorAll('li[aria-label^="Listing"]');
   var footerAds = document.querySelectorAll('div[class^="f2b12cf8"]');
   var propertyBannerAds = document.querySelectorAll(
     'div[aria-label^="Listing"]'
   );
-
+  // Loop through the <li> listings and check for a dive containing the aria label "Elite" or "Featured".
   for (let i = 0; i < listings.length; i++) {
     if (
       listings[i]
@@ -21,7 +22,7 @@ function removeads() {
     ) {
       listings[i].setAttribute("hidden", "");
     }
-  }
+  } // Loop through the <div> listings and check for a dive containing the aria label "Elite" or "Featured".
   for (let i = 0; i < footerAds.length; i++) {
     if (
       footerAds[i]
@@ -35,7 +36,7 @@ function removeads() {
     ) {
       footerAds[i].setAttribute("hidden", "");
     }
-  }
+  } // Loop through the <div> listings and check for a dive containing the aria label "Elite" or "Featured" for the properties landing page.
   for (let i = 0; i < propertyBannerAds.length; i++) {
     if (
       propertyBannerAds[i]
@@ -51,16 +52,21 @@ function removeads() {
     }
   }
 }
-removeads();
+removeads(); // Honestly, no clue why this function is called twice. The code doesnt work if it isn't there 😝.
+
+// The run() function scans for dynamic changes in the page.
 async function run() {
+  // Created a MutationObserver object to detect for changes.
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
-      removeads();
+      removeads(); // For each change the function finds, it runs the removeads() function.
     });
   });
-
+  // We create target element <head> for the observer to... well, observe.
+  // The <head> element was chosen here because, one, it mutates everytime the DOM changed, two, it has very low preformance cost.
   const targetElement = document.getElementsByTagName("head")[0];
 
+  // the observe() function is called from the observer object and take the <head> element, and the properties we want to observe.
   observer.observe(targetElement, {
     attributes: true,
     characterData: true,
@@ -70,5 +76,5 @@ async function run() {
     characterDataOldValue: true,
   });
 }
-
+// Then finally the run() function is called
 run();
